02d9a71e4c7064bdc871c049b4e41730848ba180
project-account <projeler.hesabi@gmail.com>
2018-03-19 23:54:42 +0300

    thymeleaf fixed and removed unnecessary configuration

--- src/main/java/com/campaign/test/controller/TestController.java
+++ src/main/java/com/campaign/test/controller/TestController.java
@@ -4,6 +4,7 @@ import com.campaign.test.dao.TestDao;
 import com.campaign.test.model.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +25,13 @@ public class TestController {
     }
 
     @RequestMapping(value="/mvc-test",method = RequestMethod.GET)
-    public String getMvc(){
+    public String getMvc(Model model){
+        model.addAttribute("name", "soner");
         return "test";
+    }
+
+    @RequestMapping(value="/test-mvc",method = RequestMethod.GET)
+    public String getTestMvc(Model model){
+        return "hello";
     }
 }
